270121ce3b67ed9eff1f1a984dcf7b07afa67feb
peter <>
2022-08-13 19:52:40 +0800

    init

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 import { binanceClient } from "./exchangeSetting/ExchangeConfig.js";
-import { marketInputs } from "./input/major.js";
+import { marketInputs } from "./input/major.js"; 
 import { bbSignal } from "./strategy/getBBSignal.js";
 import { getCurrentTime } from "./utilities/getCurrentTime.js";
 import { isNewBar } from "./utilities/isNewBar.js";
 import axios from "axios";
 
 const onTick = async (market, timeframe, exchange, binanceClient) => {
-  console.log(getCurrentTime());
+  // console.log(getCurrentTime());
 
   // Get BB Signal
-  const signal = await bbSignal(market, timeframe, exchange);
-  console.log("signal:", signal);
+  // const signal = await bbSignal(market, timeframe, exchange);
+  // console.log("signal:", signal);
 
   // await binanceClient.loadMarkets();
   // const btc = await binanceClient.market('BTC/USDT')
   // console.log(btc);
 
-  // const marketPrice = await binanceClient.fetchTicker(`${market}`)
-  // console.log("Bid", marketPrice.bid);
-  // console.log("ask", marketPrice.ask);
 
+  
+  // console.log(await binanceClient.fetchOHLCV(market, timeframe, undefined, 1));
+
+  console.log(await binanceClient.fetchOrders("CRV/BUSD", 1659511156353));
+ 
   // cancel exisiting orders
   // const orders = await binanceClient.fetchOpenOrders(market);
-  // console.log(orders);
+  // console.log(orders);  
   // orders.forEach(async order =>{
   //     await binanceClient.cancelOrder(order.id);
   // });
 
   // const balances = await binanceClient.fetchBalance();
-  // console.log(balances);
+  // console.log(balances);  
   // const assetBalance = balances.free[asset];
   // console.log("assetBalance",assetBalance);
   // const baseBalance = balances.free[base];
@@ -48,22 +50,16 @@ const checkNewBar = async (market, timeframe, exchange) => {
 };
 
 const run = () => {
-  setInterval(async () => {
-    if (
-      await checkNewBar(
-        marketInputs.market,
-        marketInputs.timeframe,
-        marketInputs.exchange
-      )
-    ) {
+
+ 
       onTick(
         marketInputs.market,
         marketInputs.timeframe,
         marketInputs.exchange,
         binanceClient
       );
-    }
-  }, 10000);
+
+
 };
 
 run();
